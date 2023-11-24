8ae30ad571db7ba56a10542f525e23a88a4cdca1
haonv003 <haonguyennvan@gmail.com>
2023-11-24 23:18:12 +0700

    update discount

--- src/app/components/services/cart.service.ts
+++ src/app/components/services/cart.service.ts
@@ -70,14 +70,11 @@ export class CartService {
   //   if (cartStr) {
   //     this.itemts = JSON.parse(cartStr);
   //   }
-  tt: number = 700000;
   tongtien() {
-    this.itemts.forEach(item => this.tt = this.tt + Number(item.tongTien));
-    return this.tt;
-    console.log(this.tt)
-  }
-  setTongTien(tt: number): void {
-    this.tt = tt;
+    let tt: number = 0;
+    this.itemts.forEach(item => tt = tt + Number(item.tongTien));
+    return tt;
+    console.log(tt)
   }
   clearCart() {
     this.itemts = [];
